5b5c5ece189f62967a990c0b583c58a65d33e55b
rcosnita <radu.cosnita@gmail.com>
2011-10-06 01:20:09 +0300

    I added exception mapper for not found error. Now it works really nice.

---
+++ java/ecommerce-rbac-rest-impl/src/main/java/org/ecommerce/rbac/impl/rest/RbacObjectsManagerImpl.java
@@ -0,0 +1,111 @@
+package org.ecommerce.rbac.impl.rest;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.ecommerce.rbac.api.management.RbacObjectsManager;
+import org.ecommerce.rbac.dao.SecurityObjectsDao;
+import org.ecommerce.rbac.dto.SecurityObject;
+import org.ecommerce.rbac.dto.SecurityObjects;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+Copyright (C) 2011 by Radu Viorel Cosnita
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.*/
+
+/**
+ * Rbac objects manager implementation.
+ * 
+ * @author Radu Viorel Cosnita
+ * @version 1.0
+ * @since 06.10.2011
+ */
+@Service("rbacObjectsService")
+public class RbacObjectsManagerImpl implements RbacObjectsManager {
+	private final static Logger logger = Logger.getLogger(RbacObjectsManagerImpl.class.getName());
+	
+	private SecurityObjectsDao objectsDAO;
+	
+	@Autowired
+	public RbacObjectsManagerImpl(SecurityObjectsDao objectsDAO) {
+		this.objectsDAO = objectsDAO;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public SecurityObjects loadAllObjects() {
+		logger.info("REST loading all defined RBAC objects.");
+		
+		List<org.ecommerce.rbac.persistence.entities.SecurityObject> objects = 
+			objectsDAO.loadAllObjects();
+		
+		SecurityObjects ret = new SecurityObjects();
+		
+		for(org.ecommerce.rbac.persistence.entities.SecurityObject object : objects) {
+			ret.getObjects().add(object.toSecurityObjectDTO());
+		}
+		
+		return ret;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public SecurityObject loadObjectById(Integer objectId) {
+		logger.info(String.format("REST loading RBAC object %s.", objectId));
+		
+		return objectsDAO.loadObjectById(objectId).toSecurityObjectDTO();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void createNewObject(SecurityObject object) {
+		logger.info(String.format("REST creating RBAC object %s.", object.getName()));
+		
+		objectsDAO.createNewObject(org.ecommerce.rbac.persistence.entities.SecurityObject.valueOf(object));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void updateExistingObject(Integer objectId, SecurityObject object) {
+		logger.info(String.format("REST updating RBAC object %s.", object.getName()));
+		
+		objectsDAO.updateObject(org.ecommerce.rbac.persistence.entities.SecurityObject.valueOf(object));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void removeObject(Integer objectId) {
+		logger.info(String.format("REST removing RBAC object %s.", objectId));
+		
+		objectsDAO.removeObject(objectId);
+	}
+}
---
+++ java/ecommerce-rbac-rest-impl/src/main/java/org/ecommerce/rbac/impl/rest/exceptions/NoResultExceptionMapper.java
@@ -0,0 +1,32 @@
+package org.ecommerce.rbac.impl.rest.exceptions;
+
+import javax.persistence.NoResultException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Class used to implement a mechanism that transform NoResultException
+ * into a rest exception.
+ * 
+ * @author Radu Viorel Cosnita
+ * @version 1.0
+ * @since 1.0
+ */
+@Component("noResultExceptionMapperBean")
+@Provider
+public class NoResultExceptionMapper implements ExceptionMapper<NoResultException> {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Response toResponse(NoResultException exception) {
+		Response response = 
+			Response.status(Status.NOT_FOUND).entity(exception.getMessage()).build();
+		
+		return response;
+	}
+}
